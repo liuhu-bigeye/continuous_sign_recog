eee8fbb0caf09b2d6ba76514d57ddd95699efa27
liuhu <hooglecrystal@126.com>
2017-03-20 20:14:42 +0800

    fight for journal

---
+++ all_vgg_s/main.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+
+import os
+import pdb
+import glog
+import h5py
+import numpy as np
+np.set_printoptions(precision=4)
+
+import sys
+sys.path.insert(0, '/home/liuhu/workspace/journal/')
+from loader_config import Config
+from evaluate.eval_utils import convert_pred_to_hypo, evaluate
+
+# def prob_predict(model, dataset, config, epoch):
+# 	predict_df = h5py.File(config.items['model']+'.prediction.h5', 'w')
+# 	predict_key = dataset.phase
+# 	if not predict_key in predict_df.keys():
+# 		predict_df.create_dataset(predict_key, (dataset.n_samples, dataset.max_h_len, dataset.vocabulary_size+1))
+#
+# 	begin_index = 0
+# 	do_shuffle = dataset.do_shuffle
+# 	dataset.do_shuffle = False
+# 	for index, inputs in enumerate(dataset.iterate(epoch=epoch/config.items['em_rate'])):
+# 		token = inputs[2]
+# 		prob_preds, best_path_token, ctc_loss, best_path_loss, greedy_loss = model.prob_func(*inputs)
+# 		pdb.set_trace()
+#
+# 		batch_size, max_h_len, voc_size = prob_preds.shape	# blank already at the first of axis -1
+# 		assert voc_size == dataset.vocabulary_size + 1
+#
+# 		end_index = begin_index + batch_size
+# 		predict_df[predict_key][begin_index: end_index, :max_h_len] = prob_preds
+#
+# 		if end_index%400 == 0:
+# 			glog.info('predict %d outof %d samples...' % (end_index, dataset.n_samples))
+# 		begin_index = end_index
+#
+# 	predict_df.close()
+# 	dataset.do_shuffle = do_shuffle
+
+def valid(model, valid_set, config, epoch):
+	hypos, IDs, losses = [], [], np.array([])
+	# inputs: feat, mask, label, estimate, indices, ID
+	# returns: pred_argmax
+	for valid_inputs in valid_set.iterate(epoch=epoch):
+		valid_return = model.valid_func(*valid_inputs)
+		losses = np.vstack((losses, valid_return[:-1])) if losses.shape != (0,) else np.array(valid_return[:-1])
+
+		h_len = np.sum(valid_inputs[1], axis=1)
+		hypo = convert_pred_to_hypo(valid_return[-1], h_len)
+		hypos.extend(hypo)
+		IDs.extend(valid_inputs[-1])
+
+	hypotheses = dict(zip(IDs, hypos))
+	WER = evaluate(hypotheses, valid_set.vocabulary, os.path.join(config.items['snap_path'], 'output'), epoch, valid_set.phase)
+	glog.info('%s set, Epoch %d, WER = %f, losses = %s'%(valid_set.phase, epoch, WER, np.mean(losses, axis=0)))
+	return WER
+
+
+def train_valid(model, train_set, valid_set, test_set, config):
+	min_wer = [np.inf, np.inf]	# valid_wer, test_wer
+	patienced_epoch = 0
+	min_wer_epoch = -1
+	phase = config.items['phase']
+
+	for epoch in range(config.items['starting'], config.items['max_epoch']):
+		# valid and test
+		WER_valid = valid(model, valid_set, config, epoch)
+		WER_test = valid(model, test_set, config, epoch)
+
+		# dealing with patience
+		if WER_valid < min_wer[0]:
+			min_wer = [WER_valid, WER_test]
+			min_wer_epoch = epoch
+			patienced_epoch = 0
+		else:
+			patienced_epoch += 1
+			if patienced_epoch > config.items['patience']:
+				patienced_epoch = 0
+				model.set_learning_rate(np.array(model.learning_rate.get_value() * 0.2, dtype=np.float32))
+		glog.info('No WER down for %d epoch, patienced for %d epoch, min_wer: %s' % (epoch - min_wer_epoch, patienced_epoch, min_wer))
+
+		losses = np.array([])
+
+		for iter, train_inputs in enumerate(train_set.iterate(epoch=epoch/config.items['em_rate'])):
+			train_return = model.train_func(*train_inputs)
+			losses = np.vstack((losses, train_return)) if losses.shape!=(0,) else np.array(train_return)
+			if iter % config.items['disp_iter']==0:
+				glog.info('Phase = %s, Epoch %d, Iteration %d, lr = %.2e, Training loss = %s' %
+						  (phase, epoch, iter, model.learning_rate.get_value().astype(np.float32), np.mean(losses, axis=0)))
+				model.set_learning_rate()
+				losses = np.array([])
+
+		# snapshot
+		save_file = os.path.join(config.items['snap_path'], config.items['prefix']+'_epoch_%04d' % epoch)
+		glog.info('Snap to path: %s' % save_file)
+		model.save_model(save_file)
+		model.set_learning_rate()
+	glog.info('done, min WER = dev: %f, test: %f, at epoch %d' % (min_wer[0], min_wer[1], min_wer_epoch))
+
+def main():
+	if len(sys.argv) == 3:
+		config = Config(sys.argv[1], sys.argv[2])
+	else:
+		assert False
+
+	phase = config.items['phase']
+	from utils import mkdir_safe, log_self
+	log_self(__file__)
+
+	glog.info('generating model...')
+	from model import Model
+	model = Model(phase=phase, config=config)
+
+	# load model
+	if 'model' in config.items.keys():
+		glog.info('loading model: %s...' % config.items['model'])
+		model.load_model(config.items['model'])
+	elif 'model_feat' in config.items.keys():
+		glog.info('loading feat model: %s...' % config.items['model_feat'])
+		model.load_model_feat(config.items['model_feat'])
+
+	from reader import Reader
+	train_set = Reader(phase='train', batch_size=config.items['batch_size'], do_shuffle=True, resample=True, distortion=True)
+	valid_set = Reader(phase='dev', batch_size=config.items['batch_size'], do_shuffle=False, resample=False, distortion=False)
+	test_set = Reader(phase='test', batch_size=config.items['batch_size'], do_shuffle=False, resample=False, distortion=False)
+
+	try:
+		config.items['starting'] = int(config.items['model'].split('_')[-1])
+	except:
+		config.items['starting'] = 0
+
+	# snapshot path
+	mkdir_safe(config.items['snap_path'])
+	mkdir_safe(os.path.join(config.items['snap_path'], 'output_dev'))
+	mkdir_safe(os.path.join(config.items['snap_path'], 'output_test'))
+
+	model.make_functions()
+
+	if phase == 'feat':
+		pass
+	elif phase == 'ctc':
+		glog.info('ctc training...')
+		train_valid(model, train_set, valid_set, test_set, config)
+	elif phase == 'extract_feature':
+		pass
+	elif phase == 'get_prediction':
+		pass
+
+	glog.info('end')
+
+if __name__ == '__main__':
+	main()
---
+++ all_vgg_s/reader.py
@@ -0,0 +1,142 @@
+import os
+import cv2
+import sys
+import h5py
+import random
+import pickle
+import numpy as np
+sys.path.insert(0, '/home/liuhu/workspace/fight_4_cvpr/')
+from utils import resampling, upsampling, interp_images, im_augmentation
+
+data_dir = '/home/trunk/disk1/database-rwth-2014/phoenix2014-release'
+database_file = os.path.join(data_dir, 'database_2014_combine.pkl')
+data_file = '/var/disk1/RWTH2014/cropped_right_hand.h5'
+
+class Reader(object):
+	def __init__(self, phase, batch_size, c3d_depth=4, depth_stride=4, resample=False, distortion=False, do_shuffle=False):
+		# load database
+		assert os.path.exists(database_file)
+		# log_self(__file__)
+		self.phase = phase
+		with open(database_file) as f:
+			db = pickle.load(f)
+			self.db = db[phase]
+			# {'folder': [], 'signer': [], 'annotation': [], 'vocabulary': [], 'token': [], 'begin_index': [], 'end_index': []}
+
+		self.tokens = self.db['token']
+		self.vocabulary = self.db['vocabulary']
+		self.imgs = h5py.File(data_file)['images']
+		self.batch_size = batch_size
+
+		# PCA on RGB pixels for color shifts
+		px = self.imgs[sorted(np.random.choice(799006, 1000, replace=False))]
+		px = px.reshape((-1, 3)) / 255.
+		px -= px.mean(axis=0)
+		self.eig_value, self.eig_vector = np.linalg.eig(np.cov(px.T))
+
+		self.resample = resample
+		self.distortion = distortion
+		self.do_shuffle = do_shuffle
+		self.c3d_depth = c3d_depth
+		self.depth_stride = depth_stride
+		self.vocabulary_size = len(self.db['vocabulary'])
+
+		self.n_samples = len(self.db['folder'])
+
+	def iterate_imgs(self):
+		# should only be used by cnn2d
+		img_nums = self.imgs.shape[0]
+		for k in range(0, img_nums, self.batch_size):
+			batch_size = min(self.batch_size, img_nums - k)
+			indices = range(k, k+batch_size)
+			feat = self.get_imgs(indices)
+			yield feat, k, k+batch_size
+
+	def iterate(self, return_det=False, return_folder=False):
+		index = range(self.n_samples)
+		if self.do_shuffle:
+			random.shuffle(index)
+
+		for k in range(0, self.n_samples, self.batch_size):
+			indices = index[k: k+self.batch_size]
+			batch_size = len(indices)
+
+			y_len = np.array([len(self.db['token'][i]) for i in indices], dtype=np.int32)
+			label = np.array([np.concatenate([self.tokens[idx], (y_len-len(self.tokens[idx]))*[-1]]) for idx in indices], dtype=np.int32)
+
+			ID = [self.db['folder'][i] for i in indices]
+			b_idx = self.db['begin_index']
+			e_idx = self.db['end_index']
+
+			x_len = [e_idx[i] - b_idx[i] for i in indices]
+
+			if self.resample == True:
+				X_Len, upsamp_indices = resampling(x_len, self.c3d_depth, self.depth_stride)
+				h_len = [int(np.ceil(float(l - self.c3d_depth) / float(self.depth_stride)))+1 for l in X_Len]
+			else:
+				h_len = [int(np.ceil(float(l - self.c3d_depth) / float(self.depth_stride)))+1 for l in x_len]
+				X_Len = [(l-1)*self.depth_stride + self.c3d_depth for l in h_len]
+				upsamp_indices = upsampling(x_len, self.c3d_depth, self.depth_stride)
+
+			max_h_len = np.max(h_len)
+			max_X_Len = np.max(X_Len)
+
+			feat = np.zeros((batch_size, max_X_Len, 3, 101, 101), dtype=np.float32)
+			mask = [np.concatenate((np.ones(l), np.zeros(max_h_len - l))) for l in h_len]
+
+			# gathering features
+			for i, ind in enumerate(indices):
+				feat_raw = self.get_imgs(range(b_idx[ind], e_idx[ind])) # (x_len, 3, 101, 101)
+				feat_aug = interp_images(feat_raw, upsamp_indices[i])  	# (X_Len, 3, 101, 101)	# interp_images means interp without float
+				assert X_Len[i] == feat_aug.shape[0]
+				feat[i, :X_Len[i]] = feat_aug
+				# feat[i] = np.concatenate((feat_aug, np.zeros((max_X_Len-X_Len[i], 3, 101, 101), dtype=np.float32)), axis=0)  # (max_X_Len, 1024)
+
+			feat = np.reshape(np.float32(feat), (-1, 3, 101, 101))  # (batch_size*X_len, 3, 101, 101)
+			mask = np.array(mask, dtype=np.float32)  # (batch_size, max_h_len)
+
+			yields = [feat, mask, label]
+			if return_folder:
+				yields += [ID]
+
+			yield yields
+
+	def get_imgs(self, indexs):
+		# return right hand resized
+		imgs = np.zeros((len(indexs), 101, 101, 3), dtype=np.float32)
+		mean_file = np.array([123, 117, 102], dtype=np.float32)
+		for i, index in enumerate(indexs):
+			img = self.imgs[index].astype(np.float32)
+			img = img - mean_file[None, None, :]
+			img_resized = cv2.resize(img, (101, 101))
+			imgs[i] = img_resized
+		im_augmentation(imgs, self.eig_value, self.eig_vector, trans=0.1, color_dev=0.2, distortion=self.distortion)
+		return np.transpose(imgs, [2, 0, 1])
+
+if __name__ == '__main__':
+	bs = 2
+	train_set = Reader(phase='train', batch_size=bs, c3d_depth=4, depth_stride=4, resample=True, distortion=True)
+
+	for index, (feat, mask, label, det_label, ID) in enumerate(train_set.iterate(return_det=True, return_folder=True)):
+		print feat.shape, mask.shape, det_label.shape
+
+		feat = np.reshape(feat, (bs, -1, 3, 101, 101))
+
+		idx = 0
+		img = np.transpose(feat[idx], (0, 2, 3, 1)) + np.array([123, 117, 102], dtype=np.float32)[None, None, None, :]
+
+		import skimage.io as sio
+		from skimage.transform import resize
+
+		out = np.zeros((30, 30*img.shape[0], 3), dtype=np.float32)
+
+		img[img > 255.] = 255.
+		img[img < 0.] = 0.
+
+		for i in range(img.shape[0]):
+			im = resize(img[i] / 255., (30, 30))
+			out[:, i*30: (i+1)*30, :] = im
+
+		sio.imsave('./reader.jpg', out)
+
+		break
